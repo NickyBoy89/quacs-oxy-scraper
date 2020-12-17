cdfdfc72374eef406412e683c67ea104374054a1
Nicholas Novak <nicholasmnovak@gmail.com>
2020-12-17 12:50:20 -0800

    Removed unused files and integrated the scraper into the final script

--- prerequisite_scraper/main.py
+++ prerequisite_scraper/main.py
@@ -2,10 +2,17 @@ import requests, json, re
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 from os import path
+import urllib3
 
-import course_request as courses
+import prereq_parser as prereq
 
-caching = True # Set this to true to drastically speed up requests for local development (NOTE: Must be run once to be cached)
+# Ignore the SSL errors
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+caching = True # Set this to true to speed up future requests
+
+scrapeRange = [383, 387] # What range of values that the scrape is going to do, for testing, set this smaller for it to run faster
 
 dump = {} # The dict that all the json data is going to come from
 
@@ -15,8 +22,65 @@ session = requests.Session() # Initialize the browser session
 initialLoad = session.get('https://counts.oxy.edu/public/default.aspx', verify=False)
 soup = BeautifulSoup(initialLoad.text, 'html.parser')
 
-# Breakpoint before long request
-# quit()
+# Extracts data from class
+def getClassPageData(classButton, sessionData, verbose = False):
+    postData = {
+        'ScriptManager2': f'searchResultsPanel|{classButton}',
+        'tabContainer$TabPanel1$ddlSemesters': '202101',
+        'tabContainer$TabPanel1$ddlSubjects': '',
+        'tabContainer$TabPanel1$txtCrseNum': '',
+        'tabContainer$TabPanel2$ddlCoreTerms': '201601',
+        'tabContainer$TabPanel2$ddlCoreAreas': 'CPFA',
+        'tabContainer$TabPanel2$ddlCoreSubj': 'AMST',
+        'tabContainer$TabPanel3$ddlAdvTerms': '201601',
+        'tabContainer$TabPanel3$ddlAdvSubj': 'AMST',
+        'tabContainer$TabPanel3$ddlAdvTimes': '07000755',
+        'tabContainer$TabPanel3$ddlAdvDays': 'u',
+        'tabContainer$TabPanel4$ddlCRNTerms': '201601',
+        'tabContainer$TabPanel4$txtCRN': '',
+        'tabContainer$TabPanel5$ddlMajorsTerm': '201601',
+        'tabContainer$TabPanel5$ddlCatalogYear': '201601',
+        '__EVENTTARGET': classButton,
+        '__EVENTARGUMENT': '',
+        '__LASTFOCUS': '',
+        '__VIEWSTATE': re.findall('(?<=(\|__VIEWSTATE\|))(.*?)(?=\|)', response.text)[0][1],
+        '__VIEWSTATEGENERATOR': re.findall('(?<=(\|__VIEWSTATEGENERATOR\|))(.*?)(?=\|)', response.text)[0][1],
+        '__EVENTVALIDATION': re.findall('(?<=(\|__EVENTVALIDATION\|))(.*?)(?=\|)', response.text)[0][1],
+        'tabContainer_ClientState': """{"ActiveTabIndex":0,"TabEnabledState":[true,true,true,true,true],"TabWasLoadedOnceState":[true,false,false,false,false]}""",
+        '__VIEWSTATEENCRYPTED': re.findall('(?<=(\|__VIEWSTATEENCRYPTED\|\|))(.*?)(?=\|)', response.text)[0][1],
+        '__ASYNCPOST': 'true'
+    }
+    postHeaders = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'
+    }
+    postResponse = sessionData.post(url='https://counts.oxy.edu/public/default.aspx', data=postData, headers=postHeaders)
+    classSoup = BeautifulSoup(postResponse.text, 'lxml')
+    if (verbose):
+        print(classSoup.find('span', {'id': 'lblCrseDesc'}))
+
+    restrictionsPanel = ''
+    corequisitesPanel = ''
+    prereqsPanel = ''
+
+
+    if classSoup.find(id='restrictionsPanel') != None:
+        restrictionsPanel = classSoup.find(id='restrictionsPanel').findNext('ul').findAll('li')
+
+    if classSoup.find(id='corequisitesPanel') != None:
+        corequisitesPanel = classSoup.find(id='corequisitesPanel').findNext('span').text.split(', ')
+        for i in range(len(corequisitesPanel)):
+            corequisitesPanel[i] = joinFirstTwoWordsWithDash(corequisitesPanel[i])
+
+    if classSoup.find(id='prereqsPanel') != None:
+        prereqsPanel = parsePrerequisites(classSoup.find(id='prereqsPanel').findAll('tr')[1:])
+
+    if (verbose):
+        print(f'Prerequitites: {prereqsPanel}')
+        print(f'Corequisites: {corequisitesPanel}')
+        print(f'Restrictions: {restrictionsPanel}')
+
+    return([restrictionsPanel, corequisitesPanel, prereqsPanel])
+
 
 def joinFirstTwoWordsWithDash(words):
     return(re.findall('([^\s]+\s+[^\s]+)', words)[0].replace(' ', '-'))
@@ -77,10 +141,9 @@ def parseJson(data):
         finalRestrictions['corequisites'] = {}
         finalRestrictions['corequisites']['cross_list_courses'] = data[1]
     if len(data[2]) > 0:
-        finalRestrictions['prerequisites'] = {}
-        finalRestrictions['prerequisites']['data'] = data[2]
+        finalRestrictions['prerequisites'] = prereq.parse(data[2])
 
-    print(finalRestrictions)
+    # print(finalRestrictions)
     return(finalRestrictions)
 
 data = {
@@ -119,15 +182,15 @@ headers = {
 # Gets all the course information and turns it into a soup, plus some pseudo-caching logic to speed up development
 
 if caching:
-    if path.exists('responseData.txt'):
+    if path.exists('cached_response'):
         print('Found pre-existing response data, loading that in')
-        with open('responseData.txt', 'r') as responseData:
+        with open('cached_response', 'r') as responseData:
             request = responseData.read()
         response = BeautifulSoup(request, 'lxml')
     else:
         print('Did not find cached response. Creating it now for next iteration')
         request = session.post("https://counts.oxy.edu/public/default.aspx", data=data, headers=headers, verify=False)
-        with open('responseData.txt', 'w') as responseData:
+        with open('cached_response', 'w') as responseData:
             responseData.write(request.text)
             restrictionsPanel[restriction] = re.findall('^([\w\-]+)', restrictionsPanel[restriction].text)
         response = BeautifulSoup(request.text, 'html.parser')
@@ -140,8 +203,8 @@ else:
 
 # print(testClass.find('a')['href'][25:-5])
 
-for i in tqdm((response.findAll('tr', {'style': 'background-color:#C5DFFF;font-size:X-Small;'}) + response.findAll('tr', {'style': 'background-color:White;font-size:X-Small;'}))[383:387]):
-    dump[i.find('a').text] = parseJson(courses.getClassPageData(i.find('a')['href'][25:-5], session))
+for i in tqdm((response.findAll('tr', {'style': 'background-color:#C5DFFF;font-size:X-Small;'}) + response.findAll('tr', {'style': 'background-color:White;font-size:X-Small;'}))[scrapeRange[0]:scrapeRange[1]]):
+    dump[i.find('a').text] = parseJson(getClassPageData(i.find('a')['href'][25:-5], session, verbose = False))
 
 
 
