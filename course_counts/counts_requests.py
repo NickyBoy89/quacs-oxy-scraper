fd4d6a4aa792ad817e590f59df3425a5afa61ee4
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2023-08-04 01:12:01 -0700

    change: Split the request bodies into separate files

---
+++ course_counts/counts_requests.py
@@ -0,0 +1,81 @@
+from . import CourseCountsContext
+
+
+def make_all_courses_request_body(
+    selected_semester: str,
+    context: CourseCountsContext,
+) -> Dict[str, str]:
+    return {
+        "ScriptManager2": """pageUpdatePanel|tabContainer$TabPanel1$btnGo""",
+        "tabContainer_ClientState": parsed_context.find(id="tabContainer_ClientState")[
+            "value"
+        ],
+        "__EVENTTARGET": "",
+        "__EVENTARGUMENT": "",
+        "__LASTFOCUS": "",
+        "__VIEWSTATE": context.view_state,
+        "__VIEWSTATEGENERATOR": context.view_state_generator,
+        "__VIEWSTATEENCRYPTED": context.view_state_encrypted,
+        "__EVENTVALIDATION": context.event_validation,
+        "tabContainer$TabPanel1$ddlSemesters": selected_semester,
+        "tabContainer$TabPanel1$ddlSubjects": "",
+        "tabContainer$TabPanel1$txtCrseNum": "",
+        "tabContainer$TabPanel2$ddlCoreTerms": "201601",
+        "tabContainer$TabPanel2$ddlCoreAreas": "CPFA",
+        "tabContainer$TabPanel2$ddlCoreSubj": "AMST",
+        "tabContainer$TabPanel3$ddlAdvTerms": "201601",
+        "tabContainer$TabPanel3$ddlAdvSubj": "AMST",
+        "tabContainer$TabPanel3$ddlAdvTimes": "07000755",
+        "tabContainer$TabPanel3$ddlAdvDays": "u",
+        "tabContainer$TabPanel4$ddlCRNTerms": "201601",
+        "tabContainer$TabPanel4$txtCRN": "",
+        "tabContainer$TabPanel5$ddlMajorsTerm": "201601",
+        "tabContainer$TabPanel5$ddlCatalogYear": "200801",
+        "__ASYNCPOST": "true",
+        "tabContainer$TabPanel1$btnGo": "Go",
+    }
+
+
+def make_individual_class_body(
+    parsed_class_row: NavigableString,
+    current_semester: str,
+    context: CourseCountsContext,
+):
+    # The first column contains the link to the course page
+    # The link looks something like: javascript:__doPostBack('gvResults$ctl02$lnkBtnCrn','')
+    strip_prefix = "javascript:__doPostBack('"
+    strip_suffix = "', '')"
+
+    class_button = (
+        data.find("td")
+        .find("a")["href"]
+        .removeprefix(strip_prefix)
+        .removesuffix(strip_suffix)
+    )
+
+    return {
+        "ScriptManager2": f"searchResultsPanel|{class_button}",
+        "tabContainer$TabPanel1$ddlSemesters": current_semester,
+        "tabContainer$TabPanel1$ddlSubjects": "",
+        "tabContainer$TabPanel1$txtCrseNum": "",
+        "tabContainer$TabPanel2$ddlCoreTerms": "201601",
+        "tabContainer$TabPanel2$ddlCoreAreas": "CPFA",
+        "tabContainer$TabPanel2$ddlCoreSubj": "AMST",
+        "tabContainer$TabPanel3$ddlAdvTerms": "201601",
+        "tabContainer$TabPanel3$ddlAdvSubj": "AMST",
+        "tabContainer$TabPanel3$ddlAdvTimes": "07000755",
+        "tabContainer$TabPanel3$ddlAdvDays": "u",
+        "tabContainer$TabPanel4$ddlCRNTerms": "201601",
+        "tabContainer$TabPanel4$txtCRN": "",
+        "tabContainer$TabPanel5$ddlMajorsTerm": "201601",
+        "tabContainer$TabPanel5$ddlCatalogYear": "200801",
+        "__EVENTTARGET": class_button,
+        "__EVENTARGUMENT": "",
+        "__LASTFOCUS": "",
+        "__VIEWSTATE": context.view_state,
+        "__VIEWSTATEGENERATOR": context.view_state_generator,
+        "__VIEWSTATEENCRYPTED": context.view_state_encrypted,
+        "__EVENTVALIDATION": context.event_validation,
+        "tabContainer_ClientState": """{"ActiveTabIndex":0,"TabEnabledState":[true,true,true,true,true],"TabWasLoadedOnceState":[true,false,false,false,false]}""",
+        "__ASYNCPOST": "true",
+    }
