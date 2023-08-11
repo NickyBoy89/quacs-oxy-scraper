d9d1ff8a81b8a8a2408c077128cc53dfa2678f59
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2023-08-10 23:39:35 -0700

    fix: Removed unused imports from testing

--- prerequisite_parser/test_prereqs.py
+++ prerequisite_parser/test_prereqs.py
@@ -4,8 +4,6 @@ import json
 from .parser import parse_prerequisite_list, parse_prerequisites
 from .parsed_types import ParsedPrerequisite, SingleClass, ClassGroup, Operator
 
-from .simplified import Course, Group
-
 # prerequisites = [['Math 120', 'or Math 124', 'or Math 128', 'or APBC', 'or OXMA'], ['and Phys 120', 'or Phys 125', 'or APPE']]
 # prerequisites = ['Math 101', 'and Math 100', 'and Math 201']
 # prerequisites = ["1", "and 2", "and 3", "or 4"]
