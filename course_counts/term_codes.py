a99aca5a08839511f29f83d2ecc7a6233f32c030
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2023-08-27 17:56:48 -0700

    fix: Got the sis scraper working

--- course_counts/term_codes.py
+++ course_counts/term_codes.py
@@ -8,36 +8,25 @@ class TermCode(Enum):
     Spring = 2
     Summer = 3
 
+    @staticmethod
     def from_semester_code(code: str) -> TermCode:
         # Match on the last two characters
         return TermCode(int(code[-2:]))
 
-    # These are all hardcoded by semester date
     def end_date(self) -> str:
         match self:
-            case Fall:
+            case TermCode.Fall:
                 return "11/20"
-            case Spring:
+            case TermCode.Spring:
                 return "4/27"
-            case Summer:
+            case TermCode.Summer:
                 return "8/6"
 
     def start_date(self) -> str:
         match self:
-            case Fall:
+            case TermCode.Fall:
                 return "8/24"
-            case Spring:
+            case TermCode.Spring:
                 return "1/19"
-            case Summer:
+            case TermCode.Summer:
                 return "5/1"
-
-    # Hardcoded semester start and end dates
-    if term[-2:] == "01":  # Fall
-        timeslots["dateStart"] = "8/24"
-        timeslots["dateEnd"] = "11/20"
-    elif term[-2:] == "02":  # Spring
-        timeslots["dateStart"] = "1/19"
-        timeslots["dateEnd"] = "4/27"
-    elif term[-2:] == "03":  # Summer
-        timeslots["dateStart"] = "5/1"
-        timeslots["dateEnd"] = "8/6"
